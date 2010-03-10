3f4cadb6bd73bba9360cdaa31897904781b77260
jason gagne <jgagne33@gmail.com>
2010-03-10 13:36:08 -0800

    removing migration for shipping

--- db/migrate/20100310182644_add_shipping_to_products.rb
+++
@@ -1,9 +0,0 @@
-class AddShippingToProducts < ActiveRecord::Migration
-  def self.up
-    add_column :products, :shipping, :float
-  end
-
-  def self.down
-    remove_column :products, :shipping
-  end
-end
